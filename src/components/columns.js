9142119b84480f73af1e411a6c4a426bb0f51fe1
Giftea <eleojogiftuhiene@gmail.com>
2022-08-22 11:07:00 +0100

    initial commit

---
+++ src/components/columns.js
@@ -0,0 +1,23 @@
+export const COLUMNS = [
+  {
+    header: "id",
+  },
+  {
+    header: "First Name",
+  },
+  {
+    header: "Last Name",
+  },
+  {
+    header: "email",
+  },
+  {
+    header: "DOB",
+  },
+  {
+    header: "age",
+  },
+  {
+    header: "staff",
+  },
+];
